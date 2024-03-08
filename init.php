b0a161e1d6a0e37ee934fe622d49f65413af2dca
GstavRocha <rocha.gustavo@escolar.ifrn.edu.br>
2024-03-08 11:59:07 -0300

    up2

--- init.php
+++ init.php
@@ -1,7 +1,33 @@
 <?php
-$db_host = 'db_genesis';
-$db_user = 'root';
-$db_pass =
-$db_port = 3306;
-$db_host = 'localhost';
+require_once ('config.php');
+
+header('Content-type: application/json');
+function getUsers() {
+    require_once ('config.php');
+
+    $method = $_SERVER['REQUEST_METHOD'];
+    switch ($method){
+        case 'GET':
+            try {
+                $users = $pdo->query('SELECT nome FROM tb_user');
+                $result = $users->fetchAll(PDO::FETCH_ASSOC);
+                return $result;
+            } catch (PDOException $e) {
+                echo "Erro ao executar a consulta: " . $e->getMessage();
+                return false;
+            }
+            break;
+        default:
+            echo 'deu erro';
+            return false;
+    }
+}
+
+// Exemplo de uso da função
+$usersData = getUsers();
+if ($usersData !== false) {
+    echo json_encode($usersData);
+} else {
+    echo "Houve um problema ao obter os usuários.";
+}
 ?>
